847b8529a30abcea5663fdb54cf99d0c4867fd33
Nafisa Sabnam Shama <nafisa.sabnam13@gmail.com>
2020-06-15 14:48:02 +0600

    Second commit

--- website_blocker.py
+++ website_blocker.py
@@ -4,7 +4,7 @@ from datetime import datetime as dt
 hosts_temp="E:\hosts"
 hosts_path="C:\Windows\System32\drivers\etc\hosts"
 redirect="127.0.0.1"
-website_list=["www.facebook.com","facebook.com","dub119.mail.live.com","www.dub119.mail.live.com"]
+website_list=["www.facebook.com","facebook.com","www.youtube.com"]
 
 while True:
     if dt(dt.now().year,dt.now().month,dt.now().day,16) < dt.now() < dt(dt.now().year,dt.now().month,dt.now().day,22):
